7cb8c67182b854326dae12e85aed4f7f9e7ed030
Susan Isserman <bluenote@outlook.com>
2018-11-22 18:46:03 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,6 +9,15 @@ class School
     @roster = {}
   end
 
+  def add_student(name, grade)
+    if roster[:grade]
+      roster[:grade]<< name
+    else
+      roster[:grade] = []
+      roster[:grade] << name
+    end
+  end
+
 
 
 
