a9a158febd3d438ae07afb3de0b516b332273aa5
Susan Isserman <bluenote@outlook.com>
2018-11-22 19:57:58 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+require 'pry'
+class School
+  attr_accessor :roster, :name
+  def initialize(name)
+    @name = name
+    @roster = roster
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if self.roster[grade]
+      self.roster[grade]<< name
+    else
+      self.roster[grade] = []
+      self.roster[grade] << name
+    end
+  end
+
+  def grade (grade)
+    self.roster[grade]
+  end
+  
+  def sort
+    self.roster.map do |key, value|
+      self.roster[key] = self.roster[key].sort
+    end
+    self.roster
+  end
+end
+school = School.new("Niles")
+school.add_student("Yin", 10)
+school.add_student("Susan", 10)
+
+binding.pry
